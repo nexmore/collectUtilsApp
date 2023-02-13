d9d06bf43292b4a8a16fcb5a203039c487733fae
Hyuck9 <lhg1304@gmail.com>
2023-02-13 17:20:10 +0900

    Initial commit

---
+++ src/main/java/com/datainfo/remoteshell/CollectUtilsAppApplication.java
@@ -0,0 +1,13 @@
+package com.datainfo.remoteshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectUtilsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectUtilsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/datainfo/remoteshell/CollectUtilsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datainfo.remoteshell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectUtilsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
